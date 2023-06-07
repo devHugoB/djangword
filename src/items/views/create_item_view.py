50e3b8570c28dca2e84f617744c8df24b198a22f
Théo Posty <theo@idfr.net>
2023-06-07 15:09:09 +0200

    (feat) base64 encoding

--- src/items/views/create_item_view.py
+++ src/items/views/create_item_view.py
@@ -6,6 +6,7 @@ from django.http import HttpResponseRedirect
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from string import punctuation
+import base64
 
 
 @login_required
@@ -15,16 +16,25 @@ def createItem(request):
         form = CreateItemForm(request.POST)
         if form.is_valid():
             form.cleaned_data["user_id"] = request.user.id
-            form.cleaned_data["security_index"] = test_password_strength(form.cleaned_data["password"])
+            form.cleaned_data["security_index"] = test_password_strength(
+                form.cleaned_data["password"])
+
+            form.cleaned_data["login"] = base64.b64encode(
+                form.cleaned_data["login"].encode())
+            form.cleaned_data["password"] = base64.b64encode(
+                form.cleaned_data["password"].encode())
+            form.cleaned_data["url"] = base64.b64encode(
+                form.cleaned_data["url"].encode())
+
             credential_inst = Credential.objects.create(**form.cleaned_data)
             credential_inst.save()
 
-            history = {"credential": credential_inst, 
-                            "login": credential_inst.login,
-                            "password": credential_inst.password,
-                            "url": credential_inst.url,
-                            "security_index": credential_inst.security_index,
-                            "operation": "CREATE"}
+            history = {"credential": credential_inst,
+                       "login": credential_inst.login,
+                       "password": credential_inst.password,
+                       "url": credential_inst.url,
+                       "security_index": credential_inst.security_index,
+                       "operation": "CREATE"}
             hist_inst = History.objects.create(**history)
             hist_inst.save()
 
@@ -33,10 +43,11 @@ def createItem(request):
         form = CreateItemForm()
 
     return render(
-            request,
-            "items/create_item.html",
-            context={"form": form}
+        request,
+        "items/create_item.html",
+        context={"form": form}
     )
+
 
 def test_password_strength(password: str) -> int:
     has_upper = any(c.isupper() for c in password)
@@ -44,7 +55,8 @@ def test_password_strength(password: str) -> int:
     has_digit = any(c.isdigit() for c in password)
     has_special = any(c in punctuation for c in password)
 
-    number_different_characters = sum((has_upper, has_lower, has_digit, has_special))
+    number_different_characters = sum(
+        (has_upper, has_lower, has_digit, has_special))
     score = min(number_different_characters * 2, 5)
 
     if len(password) < 10:
