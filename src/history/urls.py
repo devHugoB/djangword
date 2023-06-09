22a25bf4ed361b331151f31adc5c2377d0fdc2fc
MaxBlach <maxime.blachere@livecampus.tech>
2023-06-09 15:08:29 +0200

    Ajout de l'affichage de mot de passe dans history + lien image

--- src/history/urls.py
+++ src/history/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import history
+from .views import history, showPassword
 
 app_name = "history"
 
 urlpatterns = [
-    path("", history, name="history")
+    path("", history, name="history"),
+    path("show_password/<int:item_id>", showPassword, name="show_password")
 ]
--- src/history/views/history.py
+++ src/history/views/history.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from app.models import Credential, History
 from items.views.update_item_view import decode_item
+from django.http import JsonResponse
 
 
 @login_required
@@ -19,3 +20,8 @@ def history(request):
         "history/index.html",
         context=context
     )
+
+def showPassword(request, item_id):
+    user_credential = Credential.objects.get(pk=item_id)
+    user_credential = decode_item(user_credential)
+    return JsonResponse({"item_password": user_credential.password})
